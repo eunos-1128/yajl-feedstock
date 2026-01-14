634663fe527c254780919e30453589cd3d867f97
UENO, M. <eunosium-1128@live.jp>
2026-01-14 10:05:11 +0900

    Refactor build script to use CMake commands

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,26 +1,21 @@
+@echo on
+
 set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
-mkdir cmake-build
-cd cmake-build
-
 :: Configure.
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-      %SRC_DIR%
+cmake -S . -B build -G "NMake Makefiles JOM" %CMAKE_ARGS%
 if errorlevel 1 exit 1
 
 :: Build.
-nmake
+cmake --build build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
 
 :: Test.
-nmake test
+cmake --build build --target test %CPU_COUNT%
 if errorlevel 1 exit 1
 
 :: Install.
-nmake install
+cmake --install build %CPU_COUNT%
 if errorlevel 1 exit 1
