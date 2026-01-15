4c98d0215f4d7f3b3978d52b95fcca193f17cd36
UENO, M. <eunosium-1128@live.jp>
2026-01-15 23:34:56 +0900

    Simplify build script and enable shared libraries
    Removed conditional MacOS specific CMake arguments and enabled shared libraries.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -ex
 
-EXTRA_CMAKE_ARGS=""
-if [[ ${target_platform} == "osx-"* ]]; then
-  EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCMAKE_MACOSX_RPATH=ON"
-fi
-
-cmake -S . -B build \
-  ${CMAKE_ARGS} \
-  -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
-  ${EXTRA_CMAKE_ARGS}
-
+cmake -S . -B build ${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON
 cmake --build build --parallel ${CPU_COUNT}
 
 if [[ ${build_platform} == ${target_platform} || (${build_platform} != ${target_platform} && ${target_platform} != "osx-arm64") ]]; then 
